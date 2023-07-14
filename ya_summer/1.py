4ad09f3224b224c595103baae5985e54a9c92dbd
rashevskiivv <slava121231@mail.ru>
2023-07-14 13:03:14 +0300

    +ya summer

---
+++ ya_summer/1.py
@@ -0,0 +1,19 @@
+from datetime import timedelta
+
+n = int(input())
+times_s = []
+for i in range(n):
+    times_s.append(input())
+
+hh, mm, ss = [0] * n, [0] * n, [0] * n
+times = []
+for i in range(n):
+    hh[i], mm[i], ss[i] = times_s[i].split(":")
+    hh[i], mm[i], ss[i] = int(hh[i]), int(mm[i]), int(ss[i])
+    times.append(timedelta(hours=hh[i], minutes=mm[i], seconds=ss[i]))
+
+count = 1
+for i in range(1, n):
+    if times[i] <= times[i - 1]:
+        count += 1
+print(count)
